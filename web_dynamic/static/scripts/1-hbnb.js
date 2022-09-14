b0b8656081e306105f3e09401235994d2924eddc
MateoArbini <mateoarbini1998@gmail.com>
2022-09-14 21:47:11 +0000

    fixing 2

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,12 +1,24 @@
-#!/usr/bin/node
-// Script to be exectued when the DOM is loaded
+// let list_of_checks = [];
+// console.log("Hola")
 
-let list_of_checks = [];
-console.log("Hola")
+// $( document ).ready(function() {
+//     console.log("Se cargo el DOM")
+//     if ( $("el").is(':checked') ){
+//         if ($("#el").prop("checked")) {
+//             list_of_checks.push($("#el").attr("id"))
+//             console.log(list_of_checks) } } } );
 
-$( document ).ready(function() {
-    console.log("Se cargo el DOM")
-    if ( $("el").is(':checked') ){
-        if ($("#el").prop("checked")) {
-            list_of_checks.push($("#el").attr("id"))
-            console.log(list_of_checks) } } } );
+const idAmenity = [];
+
+console.log("holaaa");
+
+$(document).ready(function() {
+    console.log("dogchaw");
+    $("input[type=checkbox]").change(function(){
+        if ($(this).prop("checked")) {
+            idAmenity.push($(this).attr("id"))
+            console.log("aaaaaa", idAmenity)
+        }
+        console.log("ayuda")
+    })
+})
