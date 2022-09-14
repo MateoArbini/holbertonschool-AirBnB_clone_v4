6cae77b4905b4e9aaa6790c4f9ad2d9ed339eebb
MateoArbini <mateoarbini1998@gmail.com>
2022-09-14 19:43:56 +0000

    modified files for task 2

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+// Script to be exectued when the DOM is loaded
+
+let list_of_checks = [];
+
+$( document ).ready(function() {
+    if ($("#el").is(":checked")) {
+        list_of_checks.push(${data-id})
+    } else {
+        for (let i = 0; i < list_of_checks.length; i++) {
+            if (i === data-id) {
+                list_of_checks.splice(i, list_of_checks[i])
+            }
+        }
+    }
+})
