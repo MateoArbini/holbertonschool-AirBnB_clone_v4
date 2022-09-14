753711be7c569c0e8ce18d30fb49da70d98314df
MateoArbini <mateoarbini1998@gmail.com>
2022-09-14 21:01:36 +0000

    fixing2

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -6,6 +6,7 @@ let list_of_checks = [];
 $( document ).ready(function() {
     if ($("#el").is(":checked")) {
         list_of_checks.push(.attr(data-id)})
+        console.log("Hola")
     } else {
         for (let i = 0; i < list_of_checks.length; i++) {
             if (i === data-id) {
