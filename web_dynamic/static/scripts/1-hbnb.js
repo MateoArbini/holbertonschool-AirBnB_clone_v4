30105d7e7d4577a2f7a25d59451492affd755d6d
agusfl <agusflom@hotmail.com>
2022-09-15 12:22:16 +0000

    adding cganhes and comments for task 1

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,23 +1,21 @@
-// let list_of_checks = [];
-// console.log("Hola")
+const idAmenity = {};
 
-// $( document ).ready(function() {
-//     console.log("Se cargo el DOM")
-//     if ( $("el").is(':checked') ){
-//         if ($("#el").prop("checked")) {
-//             list_of_checks.push($("#el").attr("id"))
-//             console.log(list_of_checks) } } } );
-const idAmenity = [];
-
+// Load DOM first and then aply functions
 $(document).ready(function () {
-    console.log("Hola DOM cargado")
-	let checkedAmenities = {};
-	$("input[type=checkbox]").change(function () {
-	if ($(this).prop("checked")) {
-		idAmenity.push($(this).data("id"))
+  $('input[type=checkbox]').change(function () {
+    // Si se chequeo el checkbox se agrega al diccionario
+    if ($(this).prop('checked')) {
+      // Lo agregamos pasando el key (data-id) y el value (data-name)
+      idAmenity[$(this).attr('data-id')] = $(this).attr('data-name');
 	  } else {
-		idAmenity.splice(idAmenity.indexOf($(this).data("id"), 1));
+      // Si el checkbox no esta check se elimina
+      delete idAmenity[$(this).attr('data-id')];
 	  }
-      console.log("Lista elementos", idAmenity)
-	});
+    // Si el diccionario esta vacio, usamos el entity: &nbsp (non breaking space) para empezar con un espacio
+    if (idAmenity.length === 0) { $('div.amenities h4').html('&nbsp;'); }
+    // Si no esta vacia se remplaza con los elementos seleccionados
+    else {
+      $('div.amenities h4').text(Object.values(idAmenity).join(', '));
+      }
   });
+});
