e2c09eb5964af559bdf17c2e949be086270cc888
MateoArbini <mateoarbini1998@gmail.com>
2022-09-15 13:11:20 +0000

    Trying to fix semi

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,21 +1,22 @@
 const idAmenity = {};
+const $j = jQuery.noConlict();
 
 // Load DOM first and then aply functions
-$(document).ready(function () {
-  $('input[type=checkbox]').change(function () {
+$j(document).ready(function () {
+  $j('input[type=checkbox]').change(function () {
     // Si se chequeo el checkbox se agrega al diccionario
-    if ($(this).prop('checked')) {
+    if ($j(this).prop('checked')) {
       // Lo agregamos pasando el key (data-id) y el value (data-name)
-      idAmenity[$(this).attr('data-id')] = $(this).attr('data-name');
-	  } else {
+      idAmenity[$j(this).attr('data-id')] = $j(this).attr('data-name');
+    } else {
       // Si el checkbox no esta check se elimina
-      delete idAmenity[$(this).attr('data-id')];
-	  }
+      delete idAmenity[$j(this).attr('data-id')];
+    }
     // Si el diccionario esta vacio, usamos el entity: &nbsp (non breaking space) para empezar con un espacio
-    if (idAmenity.length === 0) { $('div.amenities h4').html('&nbsp;'); }
-    // Si no esta vacia se remplaza con los elementos seleccionados
-    else {
-      $('div.amenities h4').text(Object.values(idAmenity).join(', '));
-      }
+    if (idAmenity.length === 0) {
+      $j('div.amenities h4').html('&nbsp;');
+    } else {
+      $j('div.amenities h4').text(Object.values(idAmenity).join(', '));
+    }
   });
 });
