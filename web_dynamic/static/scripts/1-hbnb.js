b5e1c9c6c53f09f704861a8ae786126a3f91c2af
agusfl <agusflom@hotmail.com>
2022-09-15 13:42:44 +0000

    try js

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -12,6 +12,6 @@ $(document).ready(function () {
       // Si el checkbox no esta check se elimina
       delete idAmenity[$(this).attr('data-id')];
     }
-    $('div.amenities h4').text(idAmenity.join(', '));
+    $('div.amenities h4').text(Object.values(idAmenity).join(', '));
   });
 });
