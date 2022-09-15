719fa46c5f2d0fc583dcc3bf4ab0bb2e5a7aec86
agusfl <agusflom@hotmail.com>
2022-09-15 13:58:17 +0000

    try js

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -12,12 +12,6 @@ $(document).ready(function () {
       // Si el checkbox no esta check se elimina
       delete idAmenity[$(this).attr('data-id')];
     }
-    // Si el diccionario esta vacio, usamos el entity: &nbsp (non breaking space) para empezar con un espacio
-    if (idAmenity.length === 0) {
-      $('div.amenities h4').html('&nbsp;');
-    // Si no esta vacia se remplaza con los elementos seleccionados
-    } else {
-      $('div.amenities h4').text(Object.values(idAmenity).join(', '));
-    }
+    $('div.amenities h4').text(Object.values(idAmenity).join(', '));
   });
 });
