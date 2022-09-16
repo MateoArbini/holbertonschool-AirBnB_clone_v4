16425ec387c787a4633838da1264f20105495ca0
agusfl <agusflom@hotmail.com>
2022-09-16 14:37:17 +0000

    trying solution for task 5

--- web_dynamic/static/scripts/4-hbnb.js
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -16,7 +16,7 @@ $(document).ready(function () {
   });
   // Para testear, dejamos la IP de la sandbox, pero para el chequer debemos
   // Cambiar la IP a 0.0.0.0
-  $.get('http://924ff86aab37.a5066603.hbtn-cod.io:5001/api/v1/status/', function (data) {
+  $.get('http://0.0.0.0:5001/api/v1/status/', function (data) {
     // Si el status de la peticion es OK, agregamos la clase
     if (data.status === 'OK') {
       $('div#api_status').addClass('available');
@@ -29,7 +29,7 @@ $(document).ready(function () {
   // Task 4 - Make a POST request using AJAX:
   $.ajax({
     type: 'POST',
-    url: 'http://924ff86aab37.a5066603.hbtn-cod.io:5001/api/v1/places_search/',
+    url: 'http://0.0.0.0:5001/api/v1/places_search/',
     data: '{}',
     dataType: 'json',
     contentType: 'application/json',
@@ -56,14 +56,36 @@ $(document).ready(function () {
     }
   });
 
-  //Funcion que se ejecuta al clickear el Boton
-  const btn = document.querySelector("button");
-  btn.addEventListener("click", function() {
+  // Funcion que se ejecuta al clickear el Boton
+  const btn = document.querySelector('button');
+  btn.addEventListener('click', function () {
     $.ajax({
-        type: 'POST',
-        url: 'http://924ff86aab37.a5066603.hbtn-cod.io:5001/api/v1/places_search/',
-        data: '{}',
-        dataType: 'json',
-        contentType: 'application/json',
-  })
+      type: 'POST',
+      url: 'http://bb1c0825ee1d.e0de4a8b.hbtn-cod.io:5001/api/v1/places_search/',
+      data: idAmenity,
+      dataType: 'json',
+      contentType: 'application/json',
+      // On success we call funcition "data" y hacemos un for para "appendiar"
+      // toda la info que haya en "data" que seria todos los places
+      success: function (idAmenity) {
+        for (let i = 0; i < idAmenity.length; i++) {
+          $('section.places').append(`
+            <article>
+                <div class="title_box">
+                    <h2>${idAmenity[i].name}</h2>
+                    <div class="price_by_night">${idAmenity[i].price_by_night}</div>
+                </div>
+                <div class="information">
+                    <div class="max_guest"> ${idAmenity[i].max_guest} Guest</div>
+                        <div class="number_rooms"> ${idAmenity[i].number_rooms} Bedrooms</div>
+                        <div class="number_bathrooms"> ${idAmenity[i].number_bathrooms} Bathrooms</div>
+                    </div>
+                    <div class="user">
+                    </div>
+                        <div class="description">${idAmenity[i].description}</div>
+            </article>`);
+        }
+      }
+    });
+  });
 });
