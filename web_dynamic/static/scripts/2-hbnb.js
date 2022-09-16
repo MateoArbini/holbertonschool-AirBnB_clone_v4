ceb4e03797480ae5bf8964a3c104598f8a8681da
agusfl <agusflom@hotmail.com>
2022-09-16 02:56:27 +0000

    tying

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -16,7 +16,7 @@ $(document).ready(function () {
   });
   //Para testear, dejamos la IP de la sandbox, pero para el chequer debemos
   //Cambiar la IP a 0.0.0.0
-  $.get('http://924ff86aab37.a5066603.hbtn-cod.io:5001/api/v1/status/', function (data) {
+  $.get('http://bb1c0825ee1d.e0de4a8b.hbtn-cod.io:5001/api/v1/status/', function (data) {
     // Si el status de la peticion es OK, agregamos la clase
     if (data.status === 'OK') {
       $('div#api_status').addClass('available');
