e54842fb3c7b46767b6e95fd37b0e1cf8830978b
agusfl <agusflom@hotmail.com>
2022-09-16 17:35:38 +0000

    trying a different aproach

--- web_dynamic/static/scripts/4-hbnb.js
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -16,7 +16,7 @@ $(document).ready(function () {
   });
   // Para testear, dejamos la IP de la sandbox, pero para el chequer debemos
   // Cambiar la IP a 0.0.0.0
-  $.get('http://924ff86aab37.a5066603.hbtn-cod.io:5001/api/v1/status/', function (data) {
+  $.get('http://bb1c0825ee1d.e0de4a8b.hbtn-cod.io:5001/api/v1/status/', function (data) {
     // Si el status de la peticion es OK, agregamos la clase
     if (data.status === 'OK') {
       $('div#api_status').addClass('available');
@@ -38,8 +38,8 @@ $(document).ready(function () {
     $('section.places').empty();
     $.ajax({
       type: 'POST',
-      url: 'http://924ff86aab37.a5066603.hbtn-cod.io:5001/api/v1/places_search/',
-      data: JSON.stringify({ 'amenities': amenitiesid }), // Convertir a string
+      url: 'http://bb1c0825ee1d.e0de4a8b.hbtn-cod.io:5001/api/v1/places_search/',
+      data: JSON.stringify({ amenities: amenitiesid }), // Convertir a string
       dataType: 'json',
       contentType: 'application/json',
       // On success we call funcition "data" y hacemos un for para "appendiar"
