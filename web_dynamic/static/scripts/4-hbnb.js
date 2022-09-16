3d7fa39c9c95292489a6a0b6d1d281022f0aed8b
agusfl <agusflom@hotmail.com>
2022-09-16 15:40:57 +0000

    trying a different aproach

--- web_dynamic/static/scripts/4-hbnb.js
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -26,36 +26,6 @@ $(document).ready(function () {
     }
   });
 
-  // Task 4 - Make a POST request using AJAX:
-  $.ajax({
-    type: 'POST',
-    url: 'http://0.0.0.0:5001/api/v1/places_search/',
-    data: '{}',
-    dataType: 'json',
-    contentType: 'application/json',
-    // On success we call funcition "data" y hacemos un for para "appendiar"
-    // toda la info que haya en "data" que seria todos los places
-    success: function (data) {
-      for (let i = 0; i < data.length; i++) {
-        $('section.places').append(`
-        <article>
-            <div class="title_box">
-                <h2>${data[i].name}</h2>
-                <div class="price_by_night">${data[i].price_by_night}</div>
-            </div>
-            <div class="information">
-                <div class="max_guest"> ${data[i].max_guest} Guest</div>
-                    <div class="number_rooms"> ${data[i].number_rooms} Bedrooms</div>
-                    <div class="number_bathrooms"> ${data[i].number_bathrooms} Bathrooms</div>
-                </div>
-                <div class="user">
-                </div>
-                    <div class="description">${data[i].description}</div>
-        </article>`);
-      }
-    }
-  });
-
   // Funcion que se ejecuta al clickear el Boton
   const btn = document.querySelector('button');
   btn.addEventListener('click', function () {
